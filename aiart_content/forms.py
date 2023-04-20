39a374703e459bac93c3fd27ee7d7d09894a1ba2
Bernas <bernasfixolas@hotmail.com>
2023-04-20 16:11:14 +0100

    General Styling and fixes to post labels

--- aiart_content/forms.py
+++ aiart_content/forms.py
@@ -9,6 +9,17 @@ class ImagePostCreationForm(forms.ModelForm):
         fields = ("image_url","model",
                   "hypernetwork","positive_prompt","negative_prompt",
                   "notes","generation_details","isOnlineService","onlineService")
+        labels = {
+            "image_url":'Image URL',
+            "model":"Model",
+            "hypernetwork": "Hypernetwork",
+            "positive_promopt":"Positive Prompt",
+            "negative_prompt":"Negative Prompt",
+            "notes":"Notes",
+            "generation_details": "Generation Details",
+            "isOnlineService": "Generated in an Online Service?",
+            "onlineService": "Online Service"
+        }
 
 class ImagePostChangeForm(forms.ModelForm):
 
@@ -17,3 +28,14 @@ class ImagePostChangeForm(forms.ModelForm):
         fields = ("image_url","model",
                   "hypernetwork","positive_prompt","negative_prompt",
                   "notes","generation_details","onlineService")
+        labels = {
+            "image_url":'Image URL',
+            "model":"Model",
+            "hypernetwork": "Hypernetwork",
+            "positive_promopt":"Positive Prompt",
+            "negative_prompt":"Negative Prompt",
+            "notes":"Notes",
+            "generation_details": "Generation Details",
+            "isOnlineService": "Generated in an Online Service?",
+            "onlineService": "Online Service"
+        }

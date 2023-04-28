d6a163fd0c90390cb53f5f433e240a1a3ea2f956
Bernas <bernasfixolas@hotmail.com>
2023-04-28 19:35:05 +0100

    Improved home page

--- aiart_main/views.py
+++ aiart_main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from aiart_content.forms import SimpleSearchForm, AdvancedLocalSearchForm, AdvancedOnlineServiceSearchForm
+from aiart_content.models import ImagePost
 # Create your views here.
 
 def index(request):
     simpleform = SimpleSearchForm
     advanced_local_form = AdvancedLocalSearchForm
     advanced_online_form = AdvancedOnlineServiceSearchForm
-    context = {'simpleform':simpleform, 'advanced_local_form':advanced_local_form, 'advanced_online_form': advanced_online_form}
+    totalPosts = ImagePost.objects.count()
+    context = {'simpleform':simpleform, 'advanced_local_form':advanced_local_form, 'advanced_online_form': advanced_online_form, 'totalPosts':totalPosts}
     return render(request, template_name='index.html', context=context)

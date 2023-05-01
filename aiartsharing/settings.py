65caab91bab9c57ce1f914bbe306415aa984b5d3
Bernas <bernasfixolas@hotmail.com>
2023-05-01 17:07:37 +0100

    Hid django secret key

--- aiartsharing/settings.py
+++ aiartsharing/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^1ug^8)5-7tr5+=##s+x778gxu$-@*k)myzaekx)&b6w+8uobd'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

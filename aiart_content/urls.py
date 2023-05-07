102421d52c26869e5fb606676d6be10ba4361881
Bernas <bernasfixolas@hotmail.com>
2023-05-07 15:25:32 +0100

    Added following

--- aiart_content/urls.py
+++ aiart_content/urls.py
@@ -5,7 +5,8 @@ app_name = 'content'
 urlpatterns = [
    path('create/',views.CreateImagePostView, name='create_imagepost'),
    path('detail/<uuid:uuid>', views.DetailImagePostView.as_view(), name='detail_imagepost'),
-   path('posts/',views.ListImagePostsView.as_view(), name='list_imagepost')
+   path('posts/',views.ListImagePostsView.as_view(), name='list_imagepost'),
+   path('followingfeed/',views.ListImagePostsFollowingFeed.as_view(), name='following_feed')
 ]
 
 htmx_urlpatterns = [
